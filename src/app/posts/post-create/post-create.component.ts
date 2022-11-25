2b082a2622d5915456793a924e1177460c1ebc97
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-11-25 11:02:32 +0530

    Outputting Content

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-post-create',
+  templateUrl:'./post-create.component.html'
+})
+export class PostCreateComponent{
+  newPost="No Content";
+  onAddPost(){
+    //alert('Post Added!')
+    this.newPost='The user\'s post';
+
+  }
+
+}
